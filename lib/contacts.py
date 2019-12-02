991f38d0d5f6e5dadaf928e2cf3ca22fc62e7cc1
Steven Kim <sskim14@gmail.com>
2019-12-01 19:38:28 -0500

    Changed input options for easier readability

--- lib/contacts.py
+++ lib/contacts.py
@@ -21,10 +21,10 @@ run = "Y"
 while run == "Y":
     # Prints all the available options for navigating contacts
     selection = input("Please enter an option (enter '?' to see list of options): ").lower()
-    while selection != "?" and selection != "a" and selection != "v" and selection != "l":
+    while selection != "?" and selection != "a" and selection != "s" and selection != "v":
         selection = input("There are no options that match your selection.\nPlease enter a valid option (enter '?' to see list of options): ").lower()
     if selection == "?":
-        print("- 'a' to add a new contact\n- 'e' to edit a contact\n- 'v' to look for a contact by their name\n- 'l' to view all saved contacts")
+        print("- 'a' to add a new contact\n- 'e' to edit a contact\n- 's' to search for a contact by their name\n- 'v' to view all saved contacts")
     # Creates a new contact (name, birthdate(year, month, day), email)
     if selection == "a":
         new_key = input("Please enter a new contact name: ")
@@ -37,13 +37,13 @@ while run == "Y":
         new_key.save()
         print(f"\nSuccessfully added '{new_key.name} - {new_key.birthday} - {new_key.email}' to your contacts list.")
     # Searches for the list id of the entered name and prints the details of that id's row
-    if selection == "v":
+    if selection == "s":
         name_search = str(input("Please enter the name of the contact you are looking for: ")).capitalize()
         query = Contact.select().dicts()
         search_id = Contact.get(Contact.name == name_search).id - 1
         print(query[search_id])
     # Prints the full list of saved contacts
-    if selection == "l":
+    if selection == "v":
         query = Contact.select().dicts()
         for i in query:
             print(i)
