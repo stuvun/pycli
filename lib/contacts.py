3efcfba6280716c4adff239844a88b3da92c7d18
Steven Kim <sskim14@gmail.com>
2019-12-01 19:04:21 -0500

    Added comments and conditions for invalid inputs as well as an option to continue the application.

--- lib/contacts.py
+++ lib/contacts.py
@@ -1,11 +1,3 @@
-# 1. Ask what the user would like to do
-# 2. Inform user to enter "?" to see list of options
-#     i.   "a" for adding a new contact
-#     ii.  "e" for editing a contact
-#     iii. "d" for deleting a contact
-#     iv.  "v" to look for a contact by their first name
-#     v.   "l" to view all contacts
-
 from peewee import *
 from datetime import date
 import tkinter as tk
@@ -25,26 +17,36 @@ class Contact(BaseModel):
 
 db.connect()
 
-selection = input("Please select an option (enter '?' to see list of options): ")
-if selection == "?":
-    print("- 'a' to add a new contact\n- 'e' to edit a contact\n- 'v' to look for a contact by their name\n- 'l' to view all saved contacts")
-# Creates a new contact (name, birthdate(year, month, day), email)
-if selection == "a":
-    new_key = input("Please enter a new contact name: ")
-    new_name = str(new_key).capitalize()
-    new_year = int(input("Please enter the birth year: "))
-    new_month = int(input("Please enter the birth month (number): "))
-    new_day = int(input("Please enter the birth day: "))
-    new_email = input("Please enter the email (example1@gmail.com): ")
-    new_key = Contact(name=new_name, birthday=date(new_year, new_month, new_day), email=new_email)
-    new_key.save()
-    print(f"\nSuccessfully added '{new_key.name} - {new_key.birthday} - {new_key.email}' to your contacts list.")
-if selection == "v":
-    name_search = str(input("Please enter the name of the contact you are looking for: ")).capitalize()
-    query = Contact.select().dicts()
-    search_id = Contact.get(Contact.name == name_search).id - 1
-    print(query[search_id])
-if selection == "l":
-    query = Contact.select().dicts()
-    for i in query:
-        print(i)
+run = "y"
+while run == "y":
+    # Prints all the available options for navigating contacts
+    selection = input("Please enter an option (enter '?' to see list of options): ")
+    while selection != "?" and selection != "a" and selection != "v" and selection != "l":
+        selection = input("There are no options that match your selection.\nPlease enter a valid option (enter '?' to see list of options): ")
+    if selection == "?":
+        print("- 'a' to add a new contact\n- 'e' to edit a contact\n- 'v' to look for a contact by their name\n- 'l' to view all saved contacts")
+    # Creates a new contact (name, birthdate(year, month, day), email)
+    if selection == "a":
+        new_key = input("Please enter a new contact name: ")
+        new_name = str(new_key).capitalize()
+        new_year = int(input("Please enter the birth year: "))
+        new_month = int(input("Please enter the birth month (number): "))
+        new_day = int(input("Please enter the birth day: "))
+        new_email = input("Please enter the email (example@gmail.com): ")
+        new_key = Contact(name=new_name, birthday=date(new_year, new_month, new_day), email=new_email)
+        new_key.save()
+        print(f"\nSuccessfully added '{new_key.name} - {new_key.birthday} - {new_key.email}' to your contacts list.")
+    # Searches for the list id of the entered name and prints the details of that id's row
+    if selection == "v":
+        name_search = str(input("Please enter the name of the contact you are looking for: ")).capitalize()
+        query = Contact.select().dicts()
+        search_id = Contact.get(Contact.name == name_search).id - 1
+        print(query[search_id])
+    # Prints the full list of saved contacts
+    if selection == "l":
+        query = Contact.select().dicts()
+        for i in query:
+            print(i)
+    run = input("Would you like to do something else? (Y/N): ").capitalize()
+    while run != "Y" and run != "N":
+        run = input("Please enter 'Y' or 'N': ")
